e14f84ffea9fbcfac13b0d900ca5c0a81e78805a
FRED NYAKAGWA <fnyakagwa@mo-de.local>
2020-11-27 00:44:38 +0300

    Using component state

---
+++ src/Components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is the Todo List App and part of 
+                this coding along thanks to Traversy Media
+            </p>           
+        </div>
+    )
+}
